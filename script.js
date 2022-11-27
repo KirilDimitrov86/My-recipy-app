ed5f388fb117cf4ec8118ae270483c13bd1b87b4
KirilDimitrov86 <92857404+KirilDimitrov86@users.noreply.github.com>
2022-11-27 17:52:55 +0200

    fetching and visualizing images

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-console.log("start");
+const apiBaseUrl = "https://www.themealdb.com/api/json/v1/1";
+const categoriesEndpoint = "/categories.php";
+
+const categoriesFilterDiv = document.getElementById(
+  "detailed-categories-filter"
+);
+
+async function fetchData(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function createCategoryElement(categoryObj) {
+  console.log(categoryObj);
+
+  const { strCategory, strCategoryThumb } = categoryObj;
+  const categoryDiv = document.createElement("div");
+  categoryDiv.className = "category-box";
+
+  const categotyThumb = document.createElement("img");
+  categotyThumb.setAttribute("src", strCategoryThumb);
+  categotyThumb.setAttribute("alt", `${strCategory} category image`);
+
+  categoryDiv.appendChild(categotyThumb);
+  return categoryDiv;
+}
+
+async function main() {
+  const { categories } = await fetchData(apiBaseUrl + categoriesEndpoint);
+  console.log(categories);
+
+  categories.forEach((el) => {
+    const newCategoryEl = createCategoryElement(el);
+    categoriesFilterDiv.appendChild(newCategoryEl);
+  });
+}
+
+main();
